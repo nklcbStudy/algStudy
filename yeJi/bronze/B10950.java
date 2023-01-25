7c914e956f88069d91db0d3976e7212e527fe8c0
Jeong Yeji <wjddpwl1222@gmail.com>
2023-01-26 00:47:16 +0900

    [+]Add bronze day0125

---
+++ yeJi/bronze/B10950.java
@@ -0,0 +1,22 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		while (t > 0) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println(a + b);
+			t--;
+		}
+
+	}
+
+}
