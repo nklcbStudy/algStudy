32536e20a5b1aaa8dce5833b07428c0fac3bb4c2
Jeong Yeji <wjddpwl1222@gmail.com>
2023-01-27 01:56:25 +0900

    [+]Add day0126

---
+++ yeJi/bronze/B2739.java
@@ -0,0 +1,18 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println(n + " * " + i + " = " + (n * i));
+		}
+
+	}
+
+}
