05fb80fa141c8d703cdd9d59853d1c164855dac7
Jeong Yeji <wjddpwl1222@gmail.com>
2023-01-29 10:19:10 +0900

    [+]Mod day0129

--- yeJi/bronze/B2675.java
+++ yeJi/bronze/B2675.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 
 public class B2675 {
 
+	// 한 번에 모아서 입력받는 문제 -> BufferedReader bf = new BufferedReader(new
+	// InputStreamReader(System.in));
+	// Enter만을 경계로 입력값을 인식 (String s = bf.readLine();)
+	// Integer = Integer.parseInt(bf.readLine());
+	// 구분하여 나누기 작업 필요
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -15,34 +21,29 @@ public class B2675 {
 
 		List<String> arr = new ArrayList<>();
 
-		if (t >= 1 && t <= 1000) {
-			while (t > 0) {
+		while (t > 0) {
 
-				int r = sc.nextInt();
-				sc.nextLine();
+			int r = sc.nextInt();
 
-				if (r >= 1 && r <= 8) {
+			String s = sc.nextLine().trim(); // nextLine은 한 줄을 입력받으므로 trim이용해서 공백 없애기
 
-					String s = sc.nextLine();
+			s1 = ""; // 다음 입력 String을 저장하기 위해 null로 초기화
 
-					if (s.length() >= 1 && s.length() <= 20) {
-						for (int i = 0; i < s.length(); i++) {
-							s1 += ((char) s.charAt(i) * r); // String의 substring 사용(시작인덱스, 끝 인덱스)
-							// String의 repeat 함수 사용 (문자열 반복)
-						}
-						System.out.println(s1);
-						arr.add(s1);
-					}
+			for (int i = 0; i < s.length(); i++) {
+				for (int j = 0; j < r; j++) {
+					s1 += s.charAt(i); // String의 substring 사용(시작인덱스, 끝 인덱스)
+					// String의 repeat 함수 사용 (문자열 반복)
 				}
-
-				t--;
 			}
 
-			for (String s : arr) {
-				System.out.println(s);
-			}
+			arr.add(s1);
+
+			t--;
 		}
 
+		for (String s : arr) {
+			System.out.println(s);
+		}
 	}
 
 }
--- yeJi/bronze/B3052.java
+++ yeJi/bronze/B3052.java
@@ -1,35 +1,37 @@
 package bronze;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class B3052 {
 
+	// 중복된 것을 찾아야할 때는 hashset를 사용하자!
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		// 숫자 입력
-		int [] arr = new int [10];
+		int[] arr = new int[10];
 		for (int i = 0; i < 10; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+
 		// 나머지 저장 배열
-		int [] na = new int [10];
+		int[] na = new int[10];
 		for (int i = 0; i < na.length; i++) {
-			na[i]=arr[i]%42;
+			na[i] = arr[i] % 42;
 		}
-		
-		// 이중for문으로 하나하나 찾아내기
-		// 개수 저장
-		int cnt = 1; // 첫 나머지 개수 1
-		for (int i = 0 ; i < na.length; i++) {
-			for (int j = i+1; j < na.length; j ++) {
-				if(na[i] != na[j]) {
-					cnt++;
-				}
-			}
+
+		// HashSet을 사용하여 중복 없애기
+		Set<Integer> naset = new HashSet<>();
+
+		// 나머지가 저장되어있는 na 배열을 set에 넣어주기
+		for (int i = 0; i < na.length; i++) {
+			naset.add(na[i]);
 		}
-		System.out.println(cnt);
+
+		System.out.println(naset.size());
 
 	}
 
